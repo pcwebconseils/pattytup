92b03a10013b2e0ecf105fddfef62251f8ec00bd
pcweb <contact@pcweb-conseils.com>
2020-06-25 16:02:02 +0200

    Navbar footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,74 +1,59 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
+      <div >
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns is-mobile">
-
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
         <nuxt />
+        <div class="navbar">
+          <ul>
+            <li>
+              <nuxt-link to="/">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              </nuxt-link>
+            </li>
+            <li>
+               <nuxt-link to="/">
+              <i class="fa fa-cutlery" aria-hidden="true"></i>
+              </nuxt-link>
+            </li>
+            <li>
+               <nuxt-link to="/">
+              <i class="fa fa-book" aria-hidden="true"></i>
+              </nuxt-link>
+            </li>
+            <li>
+               <nuxt-link to="/">
+              <i class="fa fa-address-card-o" aria-hidden="true"></i>
+              </nuxt-link>
+            </li>
+          </ul>
+          </div>
       </div>
-
-    </section>
-  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
-  }
+<style langue="css">
+
+.navbar ul{
+ overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  border-top: black solid 1px;
+
 }
-</script>
+
+ul{
+
+    padding: 5px;
+    display: flex;
+    flex-direction : row;
+    justify-content: space-around;
+
+};
+
+i {
+    color: black;
+}
+.fa {
+    color: black;
+}
+
+</style>
