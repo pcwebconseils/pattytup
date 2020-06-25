4685817c78845eb8e166d242dc45964419b60bb8
Virgile-ifa <52604808+Virgile-Ifa@users.noreply.github.com>
2020-06-25 16:50:03 +0200

    update layout
    ajout couleur bcg, taille navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-      <div >
+  <div class="content">
 
+        <nuxt />
+
+        <div class="barnav">  
           <ul>
             <li>
               <nuxt-link to="/">
@@ -16,7 +19,7 @@
                <nuxt-link to="/">
               <i class="fa fa-book" aria-hidden="true"></i>
               </nuxt-link>
-            </li>
+            </li>  
             <li>
                <nuxt-link to="/">
               <i class="fa fa-address-card-o" aria-hidden="true"></i>
@@ -24,4 +27,38 @@
             </li>
           </ul>
           </div>
+      
+  </div>
+</template>
 
+<style langue="css">
+
+body{background-color: lightblue;}
+.barnav ul{
+
+  background-color: white;
+  position: fixed;
+  overflow: hidden;
+  bottom: 0;
+  width: 100%;
+  border-top: black solid 1px;
+  list-style-type: none;
+    
+}
+
+ul{ 
+  margin-left: 0;
+    height:25vh;
+    padding: 5px;
+    display: flex; 
+    flex-direction : row; 
+    justify-content: space-around; 
+    
+    
+}
+ 
+.fa {
+    color: black;
+}
+
+</style>
