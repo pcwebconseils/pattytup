1f7a464f1d1b4e993af8e1e58b2bd461831f0e4a
Virgile-ifa <52604808+Virgile-Ifa@users.noreply.github.com>
2020-06-26 17:21:47 +0200

    ajout visuel et media queries
    ajout visuel categories recettes
    media queries
    premiere vague de commentaires

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,166 @@
 <template>
-  <div class="content" id="anchor1">
+<main>
+ <!-- barre de navigation desktop -->
+  <div class="content" id="anchor1"></div>
+  <div class="is-hidden-touch">
+          <nav class="columns navigation-d">
+            <img class="column is-2 logo" src="../assets/logo.svg" alt="Logo">
+               <ul class="column is-5 nav">
+               <li class="navlink"><a class="navhover" href="#">Accueil</a></li>
+                <li class="navlink"><a class="navhover" href="#">Produits</a></li>
+               <li class="navlink"><a class="navhover" href="#">Recettes</a></li>
+               <li class="navlink"><a class="navhover" href="#">Contact</a></li>
+               </ul>
+              <form action="#" class="column is-2 search">
+               <input class="searchbar" type="text" placeholder="Rechercher" name="search" size="7">
+             <button class="search-i" type="submit"><i class="fa fa-search"></i></button>
+           </form>
+        </nav>
+        </div>
 
+      <!-- contenu -->
         <nuxt />
-        
+      
+      <!-- footer desktop -->
 
-        <a class="retour"><nuxt-link :to="{path: '/', hash: '#anchor1'}" class="button">^</nuxt-link></a>
-        <div class="barnav">  
+
+      <!-- barre de navigation mobile/tablet -->
+        <div class="is-hidden-desktop">
+        <a href="/#anchor1" class="button retour is-hidden-desktop">^</a>
+        <div class="barnav">
           <ul class="navicone">
             <li>
               <nuxt-link to="/">
-              <i class="fa fa-home" aria-hidden="true"></i>
+              <i class="fa fa-home fa-2x" aria-hidden="true"></i>
               </nuxt-link>
             </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-cutlery" aria-hidden="true"></i>
+              <i class="fa fa-cutlery fa-2x" aria-hidden="true"></i>
               </nuxt-link>
             </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-book" aria-hidden="true"></i>
+              <i class="fa fa-book fa-2x" aria-hidden="true"></i>
               </nuxt-link>
-            </li>  
+            </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-address-card-o" aria-hidden="true"></i>
+              <i class="fa fa-address-card-o fa-2x" aria-hidden="true"></i>
               </nuxt-link>
             </li>
           </ul>
           </div>
-      
-  </div>
+        </div>
+  
+  </main>
 </template>
 
 <style langue="css">
 
+
+/* Desktop format */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+a, li {
+    position: relative;
+    text-decoration: none;
+    list-style: none;
+    color: inherit;
+}
+
+a:hover {
+    color: inherit;
+}
+
+
+
+.navigation-d {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 105%;
+    background-color: #6DB6CA;
+    justify-content: center;
+    align-items: center !important;
+    padding-top: 2%;
+    padding-bottom: 2%;
+    box-shadow: 0px 4px 5px 0px rgba(0,0,0,0.75);
+}
+
+.nav {
+    justify-content: center;
+    display: flex;
+}
+
+.navlink {
+    font-family: 'Poppins', sans-serif;
+    font-size: 17px;
+    color: white;
+    text-transform: uppercase;
+    margin-right: 4%;
+}
+
+.navhover::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    border-bottom: 3px solid white;
+    transition: 0.7s;
+}
+
+.navhover:hover::after{
+    width: 100%;
+}
+
+.logo {
+    width: 12% !important;
+    text-align: center;
+}
+
+.search {
+    text-align: center;
+}
+
+.searchbar {
+    background-color: #86BFD1;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+    font-size: 15px;
+    border: none;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    padding: 2%;
+    padding-left: 5%;
+    margin-right: -1%;
+}
+
+::placeholder {
+    color: white;
+}
+
+.search-i {
+    background-color: #86BFD1;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+    font-size: 15px;
+    border: none;
+    cursor: pointer;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    padding: 2%;
+    padding-right: 5%;
+    margin-left: -1%;
+}
+
+
+/* css mobile/tablet */
 body{
   /* background-color: lightblue; */
   height: 10000px;
@@ -48,45 +174,46 @@ body{
   width: 100%;
   border-top: black solid 1px;
   list-style-type: none;
-    
+
 }
 
        .retour, .retour:visited{
          position: fixed;
-         right: 5px;
+         right: 4%;
          bottom:25.5vh;
          text-decoration: none;
-       }   
+       }
 
        .button, .button:visited{
          background-color: white;
-         color: lightblue;
+         color: #6db6ca;
          font-size: 1.8em;
-         
-         
+
        }
 
 
        .button:hover{
-         background-color: lightblue;
+         background-color: #6db6ca;
          color: white;
          text-decoration: none;
          }
 
 
-.navicone{ 
+.navicone{
   margin-left: 0;
     height:25vh;
     padding: 5px;
-    display: flex; 
-    flex-direction : row; 
-    justify-content: space-around; 
-    
-    
+    display: flex;
+    flex-direction : row;
+    justify-content: space-around;
+
+
 }
- 
+
 .fa {
-    color: black;
+    color: #6db6ca;
 }
+
+
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,274 @@
 <template>
-
-
-<div>
-  
-  <div>
-  <p class="centre">logo</p>
-  <section class="media">
-    <div class="media-left">
-    <figure class="image is-128x128">
-   <img src="../assets/pattytup.jpg">
-   </figure>
+  <main class="">
+    <div class="root2 is-hidden-touch"></div>
+    <div>
+      <p class="centre is-hidden-desktop">logo-image</p>
+      <section class="media">
+        <div class="media-left">
+          <figure class="image is-128x128 patty">
+            <img src="../assets/pattytup.jpg" />
+          </figure>
+        </div>
+        <div class="media-content">
+          <h1 class="title centre">Pattytup <br> Tupperware et recettes</h1>
+          <h2 class="centre">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui
+            nemo soluta laboriosam.
+          </h2>
+        </div>
+      </section>
     </div>
-  <div class="media-content">
-     
-  <h1 class="title centre">Pattytup</h1>
-  <p class="centre">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui nemo soluta laboriosam.</p>
-    
-  </div>
-</section>
-</div>
-<section>
-  <br>
-  <br>
-  <h4 class="centre trait title is-4">Produits du moment</h4>
-  <p class="centre">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
- 
-</section>
-</div>
+    <section>
+      <h3 class="centre trait title is-4 ">Recettes de la semaine</h3>
+      <p class="centre">
+        Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      
+    <section>
+        <div class="columns product-section">
+            <div class="column is-3-desktop is-12-mobile product-card">
+                <img src="../assets/product.jpg" alt="Image du produit" class="img-product">
+                <span class="cat-product">Catégorie du produit</span>
+                <a href="#" class="name-product patty">Nom du produit</a>
+                <span class="price-product">15,00 €</span>
+            </div>
+            <div class="column is-3-desktop is-12-mobile product-card">
+                <img src="../assets/product2.jpg" alt="Image du produit" class="img-product">
+                <span class="cat-product">Catégorie du produit</span>
+                <a href="#" class="name-product patty">Nom du produit</a>
+                <span class="price-product">15,00 €</span>
+            </div>
+            <div class="column is-3-desktop is-12-mobile product-card">
+                <img src="../assets/product3.jpg" alt="Image du produit" class="img-product">
+                <span class="cat-product">Catégorie du produit</span>
+                <a href="#" class="name-product patty">Nom du produit</a>
+                <span class="price-product">15,00 €</span>
+            </div>
+        </div>
+        <button class="btn-more">Découvrir plus</button>
+        <div class="root"></div>
+    </section>
+    </section>
+    <section>
+      <h3 class="centre trait title is-4">Produits du moment</h3>
+      <p class="centre">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
 
+      <section>
+        <div class="columns recipe-section">
+            <div class="column is-4 recipe-card">
+                <img src="../assets/recipe.jpg" alt="Image de couverture pour la recette" class="img-recipe">
+                <span class="cat-recipe">Catégorie de la recette</span>
+                <a href="#" class="name-recipe patty">Nom de la recette</a>
+                <p class="description-recipe">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore sint natus amet, veniam voluptate
+                    ipsa voluptatem voluptates voluptatum suscipit, cumque error labore vel itaque laboriosam nobis magni
+                    excepturi eveniet libero.
+                </p>
+            </div>
+            <div class="column is-4 recipe-card">
+                <img src="../assets/recipe.jpg" alt="Image de couverture pour la recette" class="img-recipe">
+                <span class="cat-recipe">Catégorie de la recette</span>
+                <a href="#" class="name-recipe patty">Nom de la recette</a>
+                <p class="description-recipe">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore sint natus amet, veniam voluptate
+                    ipsa voluptatem voluptates voluptatum suscipit, cumque error labore vel itaque laboriosam nobis magni
+                    excepturi eveniet libero.
+                </p>
+            </div>
+        </div>
+        <button class="btn-more">Découvrir plus</button>
+    </section>
 
-  
+    </section>
+  </main>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import Card from "~/components/Card";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
 
   components: {
     Card
   }
-}
+};
 </script>
 
-
 <style lang="css">
-.centre{text-align: center;}
-.trait:before, .trait:after { content:''; display:inline-block; vertical-align: middle; width: 2rem; height: 1px; background: #000 }
-.trait:before { margin-right: .3rem }
-.trait:after { margin-left: .3rem }
+
+.patty{
+  z-index: -1;
+} 
+
+.centre {
+  text-align: center;
+}
+.trait:before,
+.trait:after {
+  content: "";
+  display: inline-block;
+  vertical-align: middle;
+  width: 2rem;
+  height: 2px;
+  background: #6db6ca;
+}
+.trait:before {
+  margin-right: 0.3rem;
+}
+.trait:after {
+  margin-left: 0.3rem;
+}
+.centre{
+  margin-bottom: 2em;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+a, li {
+    position: relative;
+    text-decoration: none;
+    list-style: none;
+    color: inherit;
+}
+
+a:hover {
+    color: inherit;
+}
+
+/* Elément pour faire de l'espace*/
+
+.root2{
+  margin-top: 9%;
+}
+
+.root {
+    margin-top: 5%;
+    margin-bottom: 5%;
+}
+
+/* Produits */
+
+section {
+    justify-content: center;
+    text-align: center;
+}
+
+.product-section {
+    justify-content: center;
+}
+
+
+.product-card {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+
+.img-product {
+    object-fit: cover !important;
+    width: 70%;
+    height: 55%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+
+.cat-product {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 17px;
+    font-weight: bold;
+    color: #6DB6CA;
+    margin-top: 1%;
+}
+
+.name-product, .price-product {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 17px;
+    font-weight: bolder;
+    margin-top: 1%;
+}
+
+
+/* Recettes */
+
+.recipe-section {
+    justify-content: center;
+}
+
+.recipe-card {
+    display: flex;
+    flex-direction: column;
+    margin-right: 1%;
+}
+
+.img-recipe {
+    object-fit: cover !important;
+    width: 100%;
+    height: 50%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.cat-recipe {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 17px;
+    font-weight: bold;
+    color: #6DB6CA;
+    margin-top: 1%;
+}
+
+.name-recipe {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 17px;
+    font-weight: bolder;
+    margin-top: 1%;
+}
+
+.description-recipe {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 17px;
+    text-align: justify;
+    margin-top: 1%;
+}
+
+/* Bouton */
+
+.btn-more {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+    font-weight: bolder;
+    text-transform: uppercase;
+    background-color: white;
+    color: #6DB6CA;
+    border: 1px solid #6DB6CA;
+    border-radius: 10px;
+    padding: 0.7%;
+    padding-left: 3%;
+    padding-right: 3%;
+    cursor: pointer;
+}
+
+.btn-more:hover {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+    font-weight: bolder;
+    text-transform: uppercase;
+    background-color: #6DB6CA;
+    color: white;
+    border: 1px solid #6DB6CA;
+    border-radius: 10px;
+    padding: 0.7%;
+    padding-left: 3%;
+    padding-right: 3%;
+    cursor: pointer;
+    transition: 0.5s;
+}
+
 </style>
